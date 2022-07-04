b6901304c3cd9f825f93a68475becffc1bf49837
liuyu-22 <729467909@qq.com>
2022-07-04 19:32:44 +0800

    准备添加动画

---
+++ src/lib/Switch.vue
@@ -0,0 +1,42 @@
+<template>
+    <button :class="{ checked }"> <span></span></button>
+</template>
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+    setup() {
+        const checked = ref(false)
+        const toggle = () => {
+            checked.value = !checked.value
+        }
+        return { checked, toggle }
+    }
+}
+</script>
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+    height: $h;
+    width: $h*2;
+    border: none;
+    background: blue;
+    border-radius: $h/2;
+    position: relative;
+}
+
+span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+}
+
+button:hover>span {
+    left: calc(100% - #{$h2} - 2px);
+}
+</style> 
