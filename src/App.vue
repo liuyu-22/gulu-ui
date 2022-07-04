55f2505b434107fb814c25d159cbcb0d98212ba3
liuyu-22 <729467909@qq.com>
2022-07-04 13:24:26 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Lynn />
+</template>
+
+<script>
+import Lynn from './components/lynn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Lynn
+  }
+}
+</script>
