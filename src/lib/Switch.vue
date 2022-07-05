7a77f6877d908aae26178cca0a01ac54c7b0658f
liuyu-22 <729467909@qq.com>
2022-07-05 23:16:22 +0800

    添加样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,42 +1,69 @@
 <template>
-    <button :class="{ checked }"> <span></span></button>
+    <button class="gulu-switch" @click="toggle" :class="{ 'gulu-checked': value }">
+        <span></span>
+    </button>
 </template>
 <script lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
-    setup() {
-        const checked = ref(false)
+    props: {
+        value: Boolean,
+    },
+    setup(props, context) {
         const toggle = () => {
-            checked.value = !checked.value
-        }
-        return { checked, toggle }
+            context.emit("update:value", !props.value);
+        };
+        return { toggle };
     }
-}
+};
 </script>
-<style lang="scss" scoped>
+
+<style lang="scss" >
 $h: 22px;
 $h2: $h - 4px;
 
-button {
+gulu-switch {
     height: $h;
-    width: $h*2;
+    width: $h * 2;
     border: none;
-    background: blue;
+    background: #bfbfbf;
     border-radius: $h/2;
     position: relative;
-}
 
-span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background: white;
-    border-radius: $h2 / 2;
-}
+    >span {
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        height: $h2;
+        width: $h2;
+        background: white;
+        border-radius: $h2 / 2;
+        transition: all 250ms;
+    }
 
-button:hover>span {
-    left: calc(100% - #{$h2} - 2px);
+    &.gulu-checked {
+        background: #1890ff;
+
+        >span {
+            left: calc(100% - #{$h2} - 2px);
+        }
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    &:active {
+        >span {
+            width: $h2 + 4px;
+        }
+    }
+
+    &.gulu-checked:active {
+        >span {
+            width: $h2 + 4px;
+            margin-left: -4px;
+        }
+    }
 }
-</style> 
+</style>
