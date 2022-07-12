e06f6a1a02449a5617c2d4e279b6679fb6d754ef
liuyu-22 <729467909@qq.com>
2022-07-12 22:07:58 +0800

    增加组件

--- src/router.ts
+++ src/router.ts
@@ -7,7 +7,7 @@ import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 
 import { h } from 'vue';
-import Markdown from './components/Markdown.vue';
+import Markdown from './components/Markdown.vue'
 const history = createWebHashHistory();
 const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
